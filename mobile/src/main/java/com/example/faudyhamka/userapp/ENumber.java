bc4e653ac411b3000f92bba90c1d44d72c260a9c
faudyhamka <faudyhamka@users.noreply.github.com>
2019-06-21 12:00:19 +0700

    Final Update

--- mobile/src/main/java/com/example/faudyhamka/userapp/ENumber.java
+++ mobile/src/main/java/com/example/faudyhamka/userapp/ENumber.java
@@ -12,20 +12,34 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ENumber extends AppCompatActivity {
     public static final String myPreference = "my_pref";
+    public static final String mypreference = "mypref";
+    public static final String inputIP = "input_IP";
     EditText enumber;
     Button butt;
     TextView num0, num1, num2, num3, num4;
-    String en0, en1, en2, en3, en4;
+    String ip, en0, en1, en2, en3, en4;
     Integer i;
     Number n = new Number();
-    SharedPreferences sharedpreferences;
+    RequestQueue queue;
+    SharedPreferences sharedPreferences, sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enumber);
+        queue = Volley.newRequestQueue(this);
 
         enumber = (EditText) findViewById(R.id.editText2);
         butt = (Button) findViewById(R.id.button);
@@ -35,12 +49,14 @@ public class ENumber extends AppCompatActivity {
         num3 = (TextView) findViewById(R.id.num3);
         num4 = (TextView) findViewById(R.id.num4);
 
-        sharedpreferences = getSharedPreferences(myPreference, Context.MODE_PRIVATE);
-        en0 = sharedpreferences.getString("number0", "");
-        en1 = sharedpreferences.getString("number1", "");
-        en2 = sharedpreferences.getString("number2", "");
-        en3 = sharedpreferences.getString("number3", "");
-        en4 = sharedpreferences.getString("number4", "");
+        sharedPreferences = getSharedPreferences(myPreference, Context.MODE_PRIVATE);
+        sharedpreferences = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
+        ip = sharedpreferences.getString(inputIP, "");
+        en0 = sharedPreferences.getString("number0", "");
+        en1 = sharedPreferences.getString("number1", "");
+        en2 = sharedPreferences.getString("number2", "");
+        en3 = sharedPreferences.getString("number3", "");
+        en4 = sharedPreferences.getString("number4", "");
         num0.setText(en0); num1.setText(en1);
         num2.setText(en2); num3.setText(en3);
         num4.setText(en4);
@@ -53,13 +69,18 @@ public class ENumber extends AppCompatActivity {
                 } else {
                     i = 0;
                     while (true) {
-                        String check = sharedpreferences.getString("number"+i, "");
+                        String check = sharedPreferences.getString("number"+i, "");
                         if (check.length() < 3) {
                             String m = enumber.getText().toString();
-                            SharedPreferences.Editor editor = sharedpreferences.edit();
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
                             editor.putString("number"+Integer.toString(i), m);
                             editor.apply();
-                            n.setNum(i, m);
+                            try {
+                                JSONObject num = new JSONObject();
+                                num.put("enumber", m);
+                                num.put("id", String.valueOf(i+1));
+                                POST("http://"+ip+":3000/enumber", num);
+                            } catch (JSONException e) {e.printStackTrace();}
                             break;
                         }
                         if (i==5) {
@@ -77,10 +98,16 @@ public class ENumber extends AppCompatActivity {
 
     public void del1(View view) {
         if (en0.length()>3) {
-            SharedPreferences.Editor editor = sharedpreferences.edit();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putString("number0", " ");
             editor.apply();
             num0.setText("");
+            try {
+                JSONObject num = new JSONObject();
+                num.put("enumber", "0");
+                num.put("id", "1");
+                POST("http://"+ip+":3000/enumber", num);
+            } catch (JSONException e) {e.printStackTrace();}
         } else {
             Toast.makeText(getApplicationContext(), "No emergency number in this row", Toast.LENGTH_SHORT).show();
         }
@@ -88,10 +115,16 @@ public class ENumber extends AppCompatActivity {
 
     public void del2(View view) {
         if (en1.length()>3) {
-            SharedPreferences.Editor editor = sharedpreferences.edit();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putString("number1", " ");
             editor.apply();
             num1.setText("");
+            try {
+                JSONObject num = new JSONObject();
+                num.put("enumber", "0");
+                num.put("id", "2");
+                POST("http://"+ip+":3000/enumber", num);
+            } catch (JSONException e) {e.printStackTrace();}
         } else {
             Toast.makeText(getApplicationContext(), "No emergency number in this row", Toast.LENGTH_SHORT).show();
         }
@@ -99,10 +132,16 @@ public class ENumber extends AppCompatActivity {
 
     public void del3(View view) {
         if (en2.length()>3) {
-            SharedPreferences.Editor editor = sharedpreferences.edit();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putString("number2", " ");
             editor.apply();
             num2.setText("");
+            try {
+                JSONObject num = new JSONObject();
+                num.put("enumber", "0");
+                num.put("id", "3");
+                POST("http://"+ip+":3000/enumber", num);
+            } catch (JSONException e) {e.printStackTrace();}
         } else {
             Toast.makeText(getApplicationContext(), "No emergency number in this row", Toast.LENGTH_SHORT).show();
         }
@@ -110,10 +149,16 @@ public class ENumber extends AppCompatActivity {
 
     public void del4(View view) {
         if (en3.length()>3) {
-            SharedPreferences.Editor editor = sharedpreferences.edit();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putString("number3", " ");
             editor.apply();
             num3.setText("");
+            try {
+                JSONObject num = new JSONObject();
+                num.put("enumber", "0");
+                num.put("id", "4");
+                POST("http://"+ip+":3000/enumber", num);
+            } catch (JSONException e) {e.printStackTrace();}
         } else {
             Toast.makeText(getApplicationContext(), "No emergency number in this row", Toast.LENGTH_SHORT).show();
         }
@@ -121,12 +166,31 @@ public class ENumber extends AppCompatActivity {
 
     public void del5(View view) {
         if (en4.length()>3) {
-            SharedPreferences.Editor editor = sharedpreferences.edit();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putString("number4", " ");
             editor.apply();
             num4.setText("");
+            try {
+                JSONObject num = new JSONObject();
+                num.put("enumber", "0");
+                num.put("id", "5");
+                POST("http://"+ip+":3000/enumber", num);
+            } catch (JSONException e) {e.printStackTrace();}
         } else {
             Toast.makeText(getApplicationContext(), "No emergency number in this row", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void POST(String URLA, JSONObject B) {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URLA, B,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {}
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) { error.printStackTrace(); }
+        });
+        request.setTag("Enumber");
+        queue.add(request);
     }
 }
